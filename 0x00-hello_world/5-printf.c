fd6578f0e60deebcaf15c152feb0b35f09c45516
Elnatan999 <krosection999@gmail.com>
2021-06-17 09:06:01 -0700

    commited again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - is a void function
+ * Return: 0 always
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
+/**
+ *main - is a void function
+ * Return: 0 always
+ */
 return (0);
 }
