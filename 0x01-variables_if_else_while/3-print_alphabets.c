31bf0e99b1f45765ca7bbec5123c43d5675e2782
Elnatan999 <krosection999@gmail.com>
2021-08-12 08:19:03 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - prints alphabet
+ * Return:0
+ */
+int main(void)
+{
+int ch;
+for ( ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for ( ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+/**
+ *  main - prints alphabet
+ * Return: 0
+ */
+return (0);
+}
