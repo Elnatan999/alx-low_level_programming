e196118aa7b4bee226eeb70011a25ec1fd6da221
Elnatan999 <krosection999@gmail.com>
2021-08-14 02:10:04 -0700

    numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints without q and e
+ * Return: 0
+ */
+int main(void)
+{
+  char ch;
+  for(ch = 'a';ch <= 'z';ch++)
+    for(ch != 'e';ch != 'q';c++)
+      putchar(ch);
+  putchar('\n');
+  /**
+   * main - prints without q and e
+   * Return: 0
+   */
+return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ * Return: 0
+ */
+int main(void)
+{
+int ch;
+for(ch = 0;ch < 10;ch++)
+printf("%d",ch);
+printf("\n");
+ /**
+ * main - print numbers
+ * Return: 0
+ */
+return (0);
+}
