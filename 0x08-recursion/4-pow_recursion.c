fb04bd18cf1fa9a69baa6c5a009eae61375f8f04
Elnatan999 <krosection999@gmail.com>
2021-10-01 06:24:36 -0700

    trail of recursion is upon us

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,19 @@
 #include "holberton.h"
 
 /**
- * factorial - returns the factorial of a given number
- * @n: input number
- * Return: factorial of input
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: number to be squared
+ * @y: power of x to be squared
+ * Return: result of x**y
  */
-int factorial(int n)
+int _pow_recursion(int x, int y)
 {
-	if (n < 0)
+	if (y >= 0)
+	{
+		if (y == 0)
+			return (1);
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
 		return (-1);
-	if (n == 1)
-		return (1);
-	return (n * factorial(n - 1));
 }
