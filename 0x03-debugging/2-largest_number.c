15203329acec81057125ce2f0866d30f9ee591c6
Elnatan999 <krosection999@gmail.com>
2021-10-01 04:11:02 -0700

    debug duck

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,24 +1,34 @@
-#include <stdio.h>
-#include "main.h"
+#include "holberton.h"
+
 /**
- * largest_number - prints largest number
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
-  int largest;
-  if (a > b && b > c)
-    {
-      largest = a;
-    }
-  else if (b > a && a > c)
-    {
-      largest = b;
-    }
-  else if (c > a && a > b)
-    {
-      largest = c;
-    }
+int largest;
 
-  return (largest);
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > c && b > a)
+{
+largest = b;
+}
+else if (c > b)
+{
+largest = c;
+}
+else
+{
+largest = b;
+}
+
+
+return (largest);
 }
