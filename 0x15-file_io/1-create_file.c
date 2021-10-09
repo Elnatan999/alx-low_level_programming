2069ccc7a3d56a0c098bba039080e056c34799f9
Elnatan999 <krosection999@gmail.com>
2021-10-09 12:09:54 +0300

    commited twice

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,36 @@
 #include "holberton.h"
 
 /**
- * create_file - creates a file
- * @filename: pointer to filename
- * @text_content: string that is appended to file
- * Return: 1 on success, -1 on failure
- */
+ * create_file - a function that creates a file
+ *
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ *
+ * Return: 1 on success OR -1 on faliure
+*/
 int create_file(const char *filename, char *text_content)
 {
-	register int fd, w, len = 0;
+	int file, write_status, words = 0;
 
-	if (!filename)
+	if (filename == NULL) /*check if filename is present*/
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
+
+	/*open file by creating it and if it exists write but truncate to 0*/
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1) /*check if file creation was a success*/
 		return (-1);
-	if (text_content)
+
+	if (text_content) /*write content to file if its not NULL*/
 	{
-		while (text_content[len])
-			len++;
-		w = write(fd, text_content, len);
-		if (w == -1)
+		while (text_content[words] != '\0') /*find number of words*/
+			words++;
+
+		/*write to file*/
+		write_status = write(file, text_content, words);
+		if (write_status == -1) /*check if write was a success*/
 			return (-1);
 	}
-	close(fd);
+
+	close(file); /*close file*/
 	return (1);
 }
