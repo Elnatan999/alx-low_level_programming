868407e0e00fb5ef26f5ee3db2ff38da1d5ccb21
Elnatan999 <krosection999@gmail.com>
2021-10-09 12:20:32 +0300

    commit 3 times

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,41 +1,76 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
- * main - copies the content of a file to another file
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 if success
+ * __exit - prints error messages and exits with exit value
+ * @error: num is either exit value or file descriptor
+ * @s: str is a name, either of the two filenames
+ * @fd: file descriptor
+ * Return: 0 on success
+ **/
+int __exit(int error, char *s, int fd)
+{
+	switch (error)
+	{
+	case 97:
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(error);
+	case 98:
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+		exit(error);
+	case 99:
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+		exit(error);
+	case 100:
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(error);
+	default:
+		return (0);
+	}
+}
+
+/**
+ * main - copies one file to another
+ * @argc: should be 3 (./a.out copyfromfile copytofile)
+ * @argv: first is file to copy from (fd_1), second is file to copy to (fd_2)
+ * Return: 0 (success), 97-100 (exit value errors)
  */
 int main(int argc, char *argv[])
 {
-	int fd1, fd2;
-	int r, c1, c2;
-	char buffer[1024];
+	int fd_1, fd_2, n_read, n_wrote;
+	char *buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 < 0)
+		__exit(97, NULL, 0);
+
+	/*sets file descriptor for copy-to file*/
+	fd_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (fd_2 == -1)
+		__exit(99, argv[2], 0);
+
+	/*sets file descriptor for copy-from file*/
+	fd_1 = open(argv[1], O_RDONLY);
+	if (fd_1 == -1)
+		__exit(98, argv[1], 0);
+
+	/*reads original file as long as there's more than 0 to read*/
+	/*copies/writes contents into new file */
+	while ((n_read = read(fd_1, buffer, 1024)) != 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		if (n_read == -1)
+			__exit(98, argv[1], 0);
+
+		n_wrote = write(fd_2, buffer, n_read);
+		if (n_wrote == -1)
+			__exit(99, argv[2], 0);
 	}
-	fd2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	while ((r = read(fd1, buffer, 1024)) > 0)
-	{
-		if (fd2 < 0 || (write(fd2, buffer, r) != r))
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	}
-	if (r < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	c1 = close(fd1);
-	if (c1 < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd1), exit(100);
-	c2 = close(fd2);
-	if (c2 < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd2), exit(100);
+
+	close(fd_2) == -1 ? (__exit(100, NULL, fd_2)) : close(fd_2);
+	close(fd_1) == -1 ? (__exit(100, NULL, fd_1)) : close(fd_1);
 	return (0);
 }
